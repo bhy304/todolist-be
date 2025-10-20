49e90eba5ac878066f0667c6c24a8eaf95ee911f
Hayeon Baek <bhy0512@gmail.com>
2025-10-21 00:31:48 +0900

    fix: 배포 테스트용 todos 전체 조회 API 추가

--- routes/todos.js
+++ routes/todos.js
@@ -7,11 +7,22 @@ const {
   validateUpdateTodo,
   validateDeleteTodo,
 } = require('../middleware/validators');
+const pool = require('../mariadb');
 
 router.use(express.json());
 
 router
   .route('/')
+  .get((req, res) => {
+    const sql = 'SELECT * FROM todos';
+    pool.query(sql, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).end();
+      }
+      res.status(200).json(results);
+    });
+  })
   .get(validateGetTodos, todoController.getTodos)
   .post(validateCreateTodo, todoController.createTodo);
 
